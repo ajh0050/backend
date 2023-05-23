8c0c6b9a9b3a51354012f7656cba4fd262370fd9
Adam Hughes <adamjordanhughes@gmail.com>
2023-05-23 15:25:05 -0400

    Use the workout model in a post request to update the database with a new workout

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,30 +1,38 @@
 const express = require('express')
+const Workout = require('../models/workoutModel.js')
 
 const router = express.Router()
 
 // get all workouts
-router.get('/', (req,res) => {
-    res.json({mssg: 'GET all workouts'})
+router.get('/', (req, res) => {
+    res.json({ mssg: 'GET all workouts' })
 })
 
 // get a single workout
-router.get('/:id', (req,res) => {
-    res.json({mssg: 'Get a single workout'})
+router.get('/:id', (req, res) => {
+    res.json({ mssg: 'Get a single workout' })
 })
 
 // post a new workout
-router.post('/', (req,res) => {
-    res.json({mssg: 'post a new workout'})
+router.post('/', async (req, res) => {
+    const { title, load, reps } = req.body
+
+    try {
+        const workout = await Workout.create({ title, load, reps })
+        res.status(200).json(workout)
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
 })
 
 // delete a new workout
-router.delete('/:id', (req,res) => {
-    res.json({mssg: 'delete a new workout'})
+router.delete('/:id',  (req, res) => {
+    res.json({ mssg: 'delete a new workout' })
 })
 
 // update a new workout
-router.patch('/:id', (req,res) => {
-    res.json({mssg: 'update a new workout'})
+router.patch('/:id', (req, res) => {
+    res.json({ mssg: 'update a new workout' })
 })
 
 module.exports = router 
