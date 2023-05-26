d253ca8680eb35e79f8344eed98970b910b9bd7e
Adam Hughes <adamjordanhughes@gmail.com>
2023-05-26 18:23:06 -0400

    Add controller functions for the routes - add error handling for ID via mongoose types

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,29 +1,19 @@
 const express = require('express')
-const Workout = require('../models/workoutModel.js')
-
+const {
+    getWorkouts,
+    getWorkout,
+    createWorkout
+} = require('../controllers/workoutController')
 const router = express.Router()
 
 // get all workouts
-router.get('/', (req, res) => {
-    res.json({ mssg: 'GET all workouts' })
-})
+router.get('/', getWorkouts)
 
 // get a single workout
-router.get('/:id', (req, res) => {
-    res.json({ mssg: 'Get a single workout' })
-})
+router.get('/:id', getWorkout)
 
 // post a new workout
-router.post('/', async (req, res) => {
-    const { title, load, reps } = req.body
-
-    try {
-        const workout = await Workout.create({ title, load, reps })
-        res.status(200).json(workout)
-    } catch (error) {
-        res.status(400).json({ error: error.message })
-    }
-})
+router.post('/', createWorkout)
 
 // delete a new workout
 router.delete('/:id',  (req, res) => {
